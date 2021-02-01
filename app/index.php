ffdda245e6715f2681aaa9a99c9f76b6c1713f34
Mona Lisa <1ngvar@pm.me>
2021-02-01 17:52:10 +0200

    Finished Design page

--- app/index.php
+++ app/index.php
@@ -93,12 +93,12 @@
 			</div>
 			<div class="col-lg-6">
 				<div class="text-wrap">
-					<h2>Design Development</h2>
+					<h2><a href="desdev.php">Design Development</a></h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid assumenda at, commodi deserunt dolorem dolorum ea eaque fugit impedit laboriosam, nam nobis non nostrum obcaecati odit optio quia quibusdam, repudiandae tempora unde ut voluptas? Adipisci, atque autem, cupiditate debitis dolor dolores eaque exercitationem molestias nulla quam quo rem repudiandae vel? Cupiditate illo laudantium minima necessitatibus odio quas quasi. Cumque.
 					<br><br>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid assumenda at, commodi deserunt dolorem dolorum ea eaque fugit impedit laboriosam, nam nobis non nostrum obcaecati odit optio quia quibusdam, repudiandae tempora unde ut voluptas? Adipisci, atque autem, cupiditate debitis dolor dolores eaque exercitationem molestias nulla quam quo rem repudiandae vel? Cupiditate illo laudantium minima necessitatibus odio quas quasi. Cumque.</p>
 					<div class="btn-wrap">
-						<a href="#" class="btn-more"><svg fill="#2E6B3E" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 451.846 451.847"><path d="M345.441 248.292L151.154 442.573c-12.359 12.365-32.397 12.365-44.75 0-12.354-12.354-12.354-32.391 0-44.744L278.318 225.92 106.409 54.017c-12.354-12.359-12.354-32.394 0-44.748 12.354-12.359 32.391-12.359 44.75 0l194.287 194.284c6.177 6.18 9.262 14.271 9.262 22.366 0 8.099-3.091 16.196-9.267 22.373z"/></svg></a>
+						<a href="desdev.php" class="btn-more"><svg fill="#2E6B3E" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 451.846 451.847"><path d="M345.441 248.292L151.154 442.573c-12.359 12.365-32.397 12.365-44.75 0-12.354-12.354-12.354-32.391 0-44.744L278.318 225.92 106.409 54.017c-12.354-12.359-12.354-32.394 0-44.748 12.354-12.359 32.391-12.359 44.75 0l194.287 194.284c6.177 6.18 9.262 14.271 9.262 22.366 0 8.099-3.091 16.196-9.267 22.373z"/></svg></a>
 					</div>
 				</div>
 			</div>
